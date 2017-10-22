0b40fa7803fdec0ec26a5da200b083dc3ccb77ff
Everest Law <everestl@usc.edu>
2017-10-22 01:50:30 -0700

    Laying the ground work for two different optimization methods. See TODO comments in GooSave_demo.py for details

--- GooSave_demo.py
+++ GooSave_demo.py
@@ -11,41 +11,42 @@ def main(origin, destination, departAt = datetime.now()):
     # Create google object and make a query
     mapObj = MapWrapper(google_key)
     uberObj = UberWrapper(uber_token)
-    
+
     # Retrieve the latitudes and longitudes of origin and destination, for later use
     origin_c = mapObj.geocode(origin)
     destination_c = mapObj.geocode(destination)
-    
+
     # ___ Google: query for directions  ___ #
     end2end_directions = mapObj.queryTrip(origin, destination, departAt,
                                           preferRail=False)
-    
+
     assert len(end2end_directions) > 0, "Google returns no suggestions. Exiting."
     print("Google has returned {} trip suggestion(s)\n".format(len(end2end_directions)))
-    
+
     # Analyze the results, and recommend any trips that take less than 30 mins
     shortTripFound = False
-    
     for advice in end2end_directions:
         totalDuration = advice['legs'][0]['duration']['value']
         if totalDuration <= 2100:
             print("Trip lasting less than 35 mins found.")
             displayTrip(advice); shortTripFound = True
             print("\n")
-    
+
     if shortTripFound:
         return
     else:
-        # Find the longest segment. 
+        # TODO: wrap relevant code in the functions optimize_time(), optimize_length
+        # Find the longest segment --- by duration AND distance, respectively
         # Then, determine the mode of transport and duration of travel
+        # If the two coincide, optimize by the former (an arbitary choice)
         for advice in end2end_directions:
             end2end_duration = advice['legs'][0]['duration']['value']
-            steps, stepDurations = mapObj.extractSteps(advice) 
-            
+            steps, stepDurations = mapObj.extractSteps(advice) #TODO extract the distance travelled, too
+
             longestStepDuration = max(stepDurations)
             longestStepIndex = stepDurations.index(longestStepDuration)
             mode = mapObj.extractTransportMode(steps, longestStepIndex)
-                        
+
             print("Using only public transit, the entire trip takes {}.".format(printTime(end2end_duration)))
             print("Segment {} takes the longest, at {}, using {}.".format(longestStepIndex+1,
                                                                          printTime(longestStepDuration),
@@ -53,26 +54,26 @@ def main(origin, destination, departAt = datetime.now()):
             if mode != "WALKING":
                 transitDetails = steps[longestStepIndex]['transit_details']
                 coordKeys = ('lat', 'lng')
-                
-                departLat, departLng = [transitDetails['departure_stop']['location'][c] for c in coordKeys]
-                arriveLat, arriveLng = [transitDetails['arrival_stop']['location'][c] for c in coordKeys]
+
+                departLat, departLng = [ transitDetails['departure_stop']['location'][c] for c in coordKeys ]
+                arriveLat, arriveLng = [ transitDetails['arrival_stop']['location'][c] for c in coordKeys ]
                 departStation = transitDetails['departure_stop']['name']
-                                
+
                 print("Riding Uber to the beginning of segment {}, {}, costs".format(longestStepIndex+1,
                                                                                       departStation))
                 replacement_est = uberObj.getPrices(origin_c, (departLat, departLng))
                 printUberPrices(replacement_est)
-                
+
                 # Determine transit durations BEFORE and AFTER Uber segment
                 uberDuration = replacement_est[0]['duration'] #Same for all Uber types
                 waitDuration = uberObj.getWaitTime(origin_c)
-                                
-                afterUber_result = mapObj.queryTrip((arriveLat, arriveLng), 
-                                                    destination, 
+
+                afterUber_result = mapObj.queryTrip((arriveLat, arriveLng),
+                                                    destination,
                                                     time() + uberDuration)
-                
+
                 afterUberDuration = afterUber_result[0]['legs'][0]['duration']['value']
-                
+
                 totalDuration = [wait+uberDuration+longestStepDuration+afterUberDuration for wait in waitDuration]
                 print("All in all, the hybrid trip takes at least {}.".format(printTime(min(totalDuration))))
 
@@ -80,9 +81,9 @@ def main(origin, destination, departAt = datetime.now()):
     print("\nIn contrast, taking an Uber straight to the destination costs")
     end2end_est = uberObj.getPrices(origin_c, destination_c)
     printUberPrices(end2end_est)
-    
-    # DEBUG: return API queries for examination and understanding ''' 
+
+    # DEBUG: return API queries for examination and understanding '''
     return end2end_directions
 
-dirResult = main(origin = "University Regent, Los Angeles", 
-                 destination="Pasadena City College, CA")
+dirResult = main(origin = "Hoover / 28th, Los Angeles",
+                 destination= "Robert's Russian Cuisine, CA")
--- MapWrapper.py
+++ MapWrapper.py
@@ -11,54 +11,60 @@ class MapWrapper(object):
     def __init__(self, key):
         self.key = key
         self.gmaps = googlemaps.Client(key)
-    
+
+    def optimize_length(self, advice):
+        pass
+
+    def optimize_time(self, advice):
+        pass
+
     def geocode(self, place):
-        ''' place is a string (address or building name etc) ''' 
-        
+        ''' place is a string (address or building name etc) '''
+
         geocode_result = self.gmaps.geocode(place)
         lat = geocode_result[0]['geometry']['location']['lat']
         lng = geocode_result[0]['geometry']['location']['lng']
-                
+
         return (lat, lng)
-    
+
     def queryTrip(self, origin, destination, departAt, preferRail=False):
         if preferRail:
             mode = ["rail"]
         else:
             mode = ["rail", "bus"]
-            
+
         result = self.gmaps.directions(origin, destination, mode = "transit",
                                        transit_mode = mode,
                                        alternatives = False,
                                        departure_time = departAt)
         return result
-        
+
     def extractSteps(self, advice):
-        steps = advice['legs'][0]['steps'] 
+        steps = advice['legs'][0]['steps']
         stepDurations = [s['duration']['value'] for s in steps]
         return steps, stepDurations
-    
+
     def extractTransportMode(self, steps, index):
-        ''' 
-            Extract from a specified step mode of transit (walking / a certain public transit line)   
-        
+        '''
+            Extract from a specified step the mode of commute (walking / public transit)
+
             'steps' is a list extracted from the advice['legs'][index]['steps']
             'steps' contains detailed information on each segment of the trip
                 - where/when to begin/stop
                 - transfer instructions (with human readable text)
                 - the line of transit service used
         '''
-        
+
         s = steps[index]
         try:
             transit_details = s['transit_details']
-            mode = transit_details['line']['name'] 
+            mode = transit_details['line']['name']
 
             # If bus, must append additional info
             if mode == "Metro Local Line":
                 mode += ' (' + transit_details['line']['short_name'] + ')'
             return mode
-            
+
         except KeyError:
             mode = "WALKING"
             return mode
