230335cc6d0a7d5b01859e6e8a80e99f6f52bbec
Everest Law <everestl@usc.edu>
2017-12-15 15:34:03 -0800

    Added preliminary map display

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
-import pickle 
 
 from MapWrapper import MapWrapper
 from UberWrapper import UberWrapper
@@ -11,8 +10,9 @@ from datetime import datetime
 
 from misc import printTime
 
-uber_token="wisG3tcaRLg2sFZ49g042Bi47RvoOgDWXs-avv8h"
-google_key="AIzaSyD6n0hcRjovaiDrMOhgFuk4iGA7SjJOS0U"
+# Set up the external APIs
+uber_token = "wisG3tcaRLg2sFZ49g042Bi47RvoOgDWXs-avv8h"
+google_key = "AIzaSyBoShQRjZZ7gJeix0CeNghXsVKyoi0DaLs"
 gmapObj = MapWrapper(google_key)
 uberObj = UberWrapper(uber_token)
 
@@ -28,14 +28,20 @@ app.layout = html.Div([
                 newline,
                 dcc.Input(id='dest', value='Destination here', type="text"),
                 html.Button(id="submit_query", children="Obtain results"),
-                html.Div(id = "output")
+                html.Div(id = "output"),
+                newline,
+                html.Iframe(id = "map", 
+                            src = f"file://test.html",
+                            width = 640,
+                            height = 480)
         ])
     
 @app.callback(
     Output("output", "children"),
     [Input("submit_query", "n_clicks")],
      [State("origin", "value"),
-      State("dest", "value")]
+      State("dest", "value"),
+     ]
     )
 def parse_hybrid(click, origin, destination):
     ''' In production code, of course we will make a function call that 
@@ -51,7 +57,7 @@ def parse_hybrid(click, origin, destination):
     begin_station = tripInfo['begin_station']
     end_station = tripInfo['end_station']
     uber_prices = tripInfo['uber_prices']
-    
+            
     return html.Div([
                 html.H3("GooSaver Trip Duration: {}\n".format(min_duration)),
                 html.P("Take Uber and connect to {} @ {}\n".format(transit_line, begin_station)),
@@ -61,7 +67,23 @@ def parse_hybrid(click, origin, destination):
                             html.Ul("\t Pool: {}".format(uber_prices[0]['POOL'])),
                             html.Ul("\t UberX {}".format(uber_prices[1]['uberX']))
                         ])
-            ])    
+            ])
+
+@app.callback(
+        Output("map", "src"),
+        [Input("submit_query", "n_clicks")],
+        [State("origin", "value"), State("dest", "value")]  
+        )     
+def paintMap(click, origin, destination):
+    origin_queryStr = ""
+    for item in origin.split():
+        origin_queryStr += item + "+"
+        
+    dest_queryStr = ""
+    for item in destination.split():
+        dest_queryStr += item + "+"
+        
+    return f"https://www.google.com/maps/embed/v1/directions?key={google_key}&mode=transit&origin={origin_queryStr[:-1]}&destination={dest_queryStr[:-1]}" 
 
 if __name__ == '__main__':
     app.run_server()
